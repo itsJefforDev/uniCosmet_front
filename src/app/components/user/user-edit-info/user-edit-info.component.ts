9dfe52e4933bbf397e545956d68ae63f906c7e40
Jeff <Jeff>
2025-05-30 04:45:31 -0500

    FIX: fix edit info

--- src/app/components/user/user-edit-info/user-edit-info.component.ts
+++ src/app/components/user/user-edit-info/user-edit-info.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiLoginService } from '../../../services/user-services/api-login/api-login.service';
 import { SharedServicesService } from '../../../services/shared-services.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-edit-info',
@@ -13,17 +14,19 @@ import { SharedServicesService } from '../../../services/shared-services.service
   styleUrl: './user-edit-info.component.css'
 })
 export class UserEditInfoComponent {
-profileForm!: FormGroup;
+  profileForm!: FormGroup;
   userId!: number;
   loading = false;
   errorMessage = '';
+  user: any = [];
+
 
   constructor(
     private fb: FormBuilder,
-    private authService: ApiLoginService, 
-    private apiUserService:ApiUserService,
-    private router: Router, private sharedServicesService:SharedServicesService
-  ) {}
+    private authService: ApiLoginService,
+    private apiUserService: ApiUserService,
+    private router: Router, private sharedServicesService: SharedServicesService
+  ) { }
 
   ngOnInit(): void {
     this.sharedServicesService.setPurchasesProduct(false);
@@ -35,6 +38,7 @@ profileForm!: FormGroup;
     }
 
     this.profileForm = this.fb.group({
+      id: 0,
       name: ['', Validators.required],
       age: ['', [Validators.required, Validators.min(1), Validators.max(120)]],
       email: ['', [Validators.required, Validators.email]],
@@ -46,6 +50,7 @@ profileForm!: FormGroup;
     this.apiUserService.getUserById(this.userId).subscribe({
       next: (user) => {
         this.profileForm.patchValue(user);
+
       },
       error: () => {
         this.errorMessage = 'No se pudo cargar la información del usuario.';
@@ -61,10 +66,16 @@ profileForm!: FormGroup;
     this.apiUserService.updateUser(this.profileForm.value).subscribe({
       next: () => {
         this.loading = false;
+        Swal.fire({
+          title: "Informacion guardada correctamente ;D",
+          icon: "success",
+          draggable: true
+        });
         this.router.navigate(['/userDashComponent']);
       },
       error: (err) => {
         this.errorMessage = err.message || 'Error al actualizar el perfil';
+        console.log(this.profileForm);
         this.loading = false;
       }
     });
